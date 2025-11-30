f97a753441eaeffde01ed82043e4cc8b0d5183d1
Daniel Dantas <contatodanieldantasdev@gmail.com>
2025-11-30 17:19:06 -0300

    feat: adição do arquivo GetAllProjectsQueryHandler.cs

---
+++ developments/devfreela-turbinado-filtro-paginacao/src/DevFreela.Application/Queries/GetAllProjects/GetAllProjectsQueryHandler.cs
@@ -0,0 +1,44 @@
+﻿using DevFreela.Application.ViewModels;
+using DevFreela.Core.Models;
+using DevFreela.Core.Repositories;
+using DevFreela.Infrastructure.Persistence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DevFreela.Application.Queries.GetAllProjects
+{
+    public class GetAllProjectsQueryHandler : IRequestHandler<GetAllProjectsQuery, PaginationResult<ProjectViewModel>>
+    {
+        private readonly IProjectRepository _projectRepository;
+        public GetAllProjectsQueryHandler(IProjectRepository projectRepository)
+        {
+            _projectRepository = projectRepository;
+        }
+
+        public async Task<PaginationResult<ProjectViewModel>> Handle(GetAllProjectsQuery request, CancellationToken cancellationToken)
+        {
+            var projectPaginationResult = await _projectRepository.GetAllAsync(request.Query, request.Page);
+
+            var projectsViewModel = projectPaginationResult
+                .Data
+                .Select(p => new ProjectViewModel(p.Id, p.Title, p.CreatedAt))
+                .ToList();
+
+            var paginationResult = new PaginationResult<ProjectViewModel>(
+               projectPaginationResult.Page,
+               projectPaginationResult.TotalPages,
+               projectPaginationResult.PageSize,
+               projectPaginationResult.ItemsCount,
+               projectsViewModel
+            );
+
+            return paginationResult;
+        }
+    }
+}
