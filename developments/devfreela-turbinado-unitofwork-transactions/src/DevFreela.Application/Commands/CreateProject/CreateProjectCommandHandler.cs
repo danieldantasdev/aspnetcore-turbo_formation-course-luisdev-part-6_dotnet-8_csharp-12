587a601e45a86577a31550ff52be82133d326636
Daniel Dantas <contatodanieldantasdev@gmail.com>
2025-11-30 17:19:47 -0300

    feat: adição do arquivo CreateProjectCommandHandler.cs

---
+++ developments/devfreela-turbinado-unitofwork-transactions/src/DevFreela.Application/Commands/CreateProject/CreateProjectCommandHandler.cs
@@ -0,0 +1,38 @@
+﻿using DevFreela.Core.Entities;
+using DevFreela.Core.Repositories;
+using DevFreela.Infrastructure.Persistence;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DevFreela.Application.Commands.CreateProject
+{
+    public class CreateProjectCommandHandler : IRequestHandler<CreateProjectCommand, int>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public CreateProjectCommandHandler(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<int> Handle(CreateProjectCommand request, CancellationToken cancellationToken)
+        {
+            var project = new Project(request.Title, request.Description, request.IdClient, request.IdFreelancer, request.TotalCost);
+            project.Comments.Add(new ProjectComment("Project was created.", project.Id, request.IdClient));
+
+            await _unitOfWork.BeginTransactionAsync();
+
+            await _unitOfWork.Projects.AddAsync(project);
+
+            await _unitOfWork.CompleteAsync();
+
+            await _unitOfWork.Skills.AddSkillFromProject(project);
+
+            await _unitOfWork.CompleteAsync();
+
+            await _unitOfWork.CommitAsync();
+
+            return project.Id;
+        }
+    }
+}
