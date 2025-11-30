ddf0dd603cb55b209fb1d3336904d8e1ed79da09
Daniel Dantas <contatodanieldantasdev@gmail.com>
2025-11-30 17:20:27 -0300

    feat: adição do arquivo SkillRepository.cs

---
+++ developments/devfreela-turbinado-unitofwork-transactions/src/DevFreela.Infrastructure/Persistence/Repositories/SkillRepository.cs
@@ -0,0 +1,37 @@
+﻿using DevFreela.Core.Entities;
+using DevFreela.Core.Repositories;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevFreela.Infrastructure.Persistence.Repositories
+{
+    public class SkillRepository : ISkillRepository
+    {
+        private readonly DevFreelaDbContext _dbContext;
+        public SkillRepository(DevFreelaDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<List<Skill>> GetAllAsync()
+        {
+            return await _dbContext.Skills.ToListAsync();
+        }
+
+        public async Task AddSkillFromProject(Project project)
+        {
+            // App Xamarin de Marketplace
+            var words = project.Description.Split(' ');
+            var length = words.Length;
+
+            var skill = $"{project.Id} - {words[length - 1]}";
+            // "1 - Marketplace"
+            
+            await _dbContext.Skills.AddAsync(new Skill(skill));
+        }
+    }
+}
